7fb01257d4fde91414262526ed3c9343e28d39ba
shamrasal <shameshrasal77@gmail.com>
2022-06-23 20:16:00 +0530

    second commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      locationOfExpendeture: "pune",
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), locationOfExpendeture: "pune" },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      locationOfExpendeture: "mumbai",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      locationOfExpendeture: "delhi",
+    },
+  ];
+  return (
+    expenses.map((expense) => {
+      return (
+        <div>
+          <ExpenseItem
+            Title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            location={expense.locationOfExpendeture}
+          ></ExpenseItem>
+        </div>
+      )
+    })
+  )
+}
+
+export default App;
