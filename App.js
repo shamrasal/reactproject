c84ace1d1c1a440278e000e5a3fafbc434182a46
shamrasal <106094203+shamrasal@users.noreply.github.com>
2022-06-24 18:37:08 +0530

    added card and arrow function

--- App.js
+++ App.js
@@ -1,44 +1,14 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      locationOfExpendeture: "pune",
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), locationOfExpendeture: "pune" },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      locationOfExpendeture: "mumbai",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      locationOfExpendeture: "delhi",
-    },
-  ];
+const App = () => {
   return (
-    expenses.map((expense) => {
-      return (
-        <div>
-          <ExpenseItem
-            Title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-            location={expense.locationOfExpendeture}
-          ></ExpenseItem>
-        </div>
-      )
-    })
+    <div>
+      <h1>lets get started</h1>
+      <div>
+        <Expenses></Expenses>
+      </div>
+    </div>
   )
-}
 
+}
 export default App;
